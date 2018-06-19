d1aab7476e856cdf528f37d8671cce74b8dafdcb
Pascal Irminger <pascal.irminger@gmail.com>
2018-06-19 16:02:30 +0200

    Learning rate

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
                 input_image: image,
                 correct_label: label,
                 keep_prob: keep_prob,
-                learning_rate: learning_rate
+                learning_rate: 1e-5
             })
             print('Epoch: {}, loss: {}'.format(epoch + 1, loss))
             print()
