af5630eb75b958194d4328f0148f187707777026
Pascal Irminger <pascal.irminger@gmail.com>
2018-06-19 15:55:46 +0200

    Call `softmax_cross_entropy_with_logits` with named arguments

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     correct_label = tf.reshape(correct_label, (-1, num_classes))
     # Use standard cross entropy loss
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, correct_label))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
     # Define optimizer
     optimizer = tf.train.AdamOptimizer(learning_rate)
     # Apply optimizer to the loss function
