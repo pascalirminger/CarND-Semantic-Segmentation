84bf601f6c4b5baeb75aed5db024db9cd295818a
Pascal Irminger <pascal.irminger@gmail.com>
2018-06-19 16:05:20 +0200

    keep_prob

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
             _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={
                 input_image: image,
                 correct_label: label,
-                keep_prob: keep_prob,
+                keep_prob: 0.5,
                 learning_rate: 0.00001
             })
             print('Epoch: {}, loss: {}'.format(epoch + 1, loss))
